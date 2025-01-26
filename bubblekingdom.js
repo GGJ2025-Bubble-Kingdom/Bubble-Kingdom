3d0556b5d1e2a64291694de2b1082a971fe1c3c1
HiTzz <renatonandes7@gmail.com>
2025-01-26 06:01:00 -0300

    Js_e_morreu/perdeu

--- bubblekingdom.js
+++ bubblekingdom.js
@@ -1,9 +1,23 @@
+// esses codigos tão inutilizados por hora(não sei se vou usar) //
+
 const inputElement = document.getElementById('userInput');
 const outputElement = document.getElementById('output');
 
+document.addEventListener('DOMContentLoaded', function() {
+
+    const nameH = localStorage.getItem('nome');
+    if (nameH){
+        inputElement.value = nameH;
+    }
+});    
+
 inputElement.addEventListener('input', function () {
 
-    const valorDigitado = inputElement.value;
+    const nome = inputElement.value;
 
-    outputElement.textContent = 'Você digitou: ${valorDigitado}';
+    outputElement.textContent = `Seu héroi se chamará: ${nome}`;
 });
+
+        const heroName = (localStorage.getItem('heroName') || 'SEU HEROI').toUpperCase();
+        const choosenName = document.getElementById('choosenName');
+        choosenName.textContent = `${heroName}`;
